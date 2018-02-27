436e88863746e1ab1a3553007638644015bf559a
lml19960131 <lml19960131@126.com>
2018-02-27 21:38:17 +0800

    gulp配置

--- tasks/util/args.js
+++ tasks/util/args.js
@@ -1,3 +1,33 @@
-/**
- * Created by 62354 on 2018/2/27.
- */
+import yargs from 'yargs';
+
+const args = yargs
+
+    .option('prouduction', {
+        boolean: true,
+        default: false,
+        describe: 'min all scripts'
+    })
+
+    .option('watch', {
+        boolean: true,
+        default: false,
+        describe: 'watch all files'
+    })
+
+    .option('verbose', {
+        boolean: true,
+        default: false,
+        describe: 'log'
+    })
+
+    .option('sourcemaps', {
+        describe: 'force the creation of sourcemmaps'
+    })
+
+    .option('port', {
+        string: true,
+        default: 8080,
+        describe: 'server port'
+    })
+
+    .argv;
